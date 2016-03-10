9e576041164a20a0d935073b0c0c7e82b6ff5ca8
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-03-10 13:35:49 -0500

    basic input validation

--- app.js
+++ app.js
@@ -3,34 +3,38 @@ $(() => {
   const input = $('#input');
   const output = $('#output');
   
-  let key, fetching;
+  let key, lastFetch;
   
-  repo.on('change', () => {
-    key = undefined;
-    fetching = false;
-    update();
-  });
+  repo.on('change', update);
   input.on('input', update);
   
   function update() {
-    if (fetching) return;
-    if (!repo.val()) return;
+    repo.css('border-color', 'red');
+    input.css('border-color', 'red');
+    output.css('border-color', 'red');
     
-    if (!key) {
-      fetching = true;
-      // deliberately avoid handling the case where this request fails
-      // so we don't end up trying to fetch it over and over again
+    if (key) {
+      repo.css('border-color', 'green');
+    } else {
+      if (lastFetch === repo.val()) return;
+      lastFetch = repo.val();
       $.getJSON(`https://api.travis-ci.org/repos/${repo.val()}/key`)
-        .then(val => {
-          key = val.key;
-          fetching = false;
-          update();
-        });
-      return;
+        .then(val => key = val.key)
+        .catch(() => {})
+        .then(update);
     }
     
-    const enc = new JSEncrypt();
-    enc.setKey(key);
-    output.val(enc.encrypt(input.val()));
+    if (input.val()) {
+      input.css('border-color', 'green');
+    }
+    
+    if (key && input.val()) {
+      const enc = new JSEncrypt();
+      enc.setKey(key);
+      output.val(enc.encrypt(input.val()));
+      output.css('border-color', 'green');
+    }
   }
+  
+  update();
 });
