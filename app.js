8f889869ca76eb5371e52e56b928af07efb3ee63
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-03-10 13:19:49 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const repo = $('#repo');
+const input = $('#input');
+const output = $('#output');
+
+let key, fetching;
+
+repo.on('change', () => {
+  key = undefined;
+  fetching = false;
+  update();
+});
+input.on('input', update);
+
+function update() {
+  if (fetching) return;
+  if (!repo.val()) return;
+  
+  if (!key) {
+    fetching = true;
+    // deliberately avoid handling the case where this request fails
+    // so we don't end up trying to fetch it over and over again
+    $.getJson(`https://api.travis-ci.org/repos/${repo.val()}/key`)
+      .then(val => {
+        key = val.key;
+        fetching = false;
+        update();
+      });
+    return;
+  }
+  
+  const enc = new JSEncrypt();
+  enc.setKey(key);
+  output.val(enc.encrypt(input.val()));
+}
